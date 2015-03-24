7c747b55e24739af89a7281affe76a06718bf2de
IgorGanapolsky <eazyigz@gmail.com>
2015-03-24 08:53:56 -0400

    Update gradle project in Android Studio.

--- Chapter 15 - Sensors/SimpleHardware/src/com/advancedandroidbook/simplehardware/SimpleHardwareActivity.java
+++
@@ -1,9 +0,0 @@
-package com.advancedandroidbook.simplehardware;
-
-public class SimpleHardwareActivity extends MenuActivity {
-	@Override
-	void prepareMenu() {
-		addMenuItem("1. Sensors Sample", SensorsActivity.class);
-		addMenuItem("2. Battery Monitor", BatteryActivity.class);
-	}
-}
